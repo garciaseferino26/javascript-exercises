0d3b45109c311340d82dbd9f16e5a7840cc89a3f
Seferino Garcia <garciaseferino26@gmail.com>
2023-06-28 17:05:57 -0500

    solved promblem 06 leap year using functions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(a) {
+    if (a%4 != 0) {
+        return false;
+    } else if ((a%4 == 0) && (a%100 == 0) && (a%400 ==0)) {
+        return true;
+    }   else if ((a%4 == 0) && (a%100 == 0) && (!a%400 ==0)) {
+        return false;
+    }   else if ((a%4 == 0) && (!a%100 == 0) && (!a%400 ==0)) {
+        return true;
+    }
 };
 
 // Do not edit below this line

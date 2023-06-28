4815322e77ae774a7074491a4278110452f44b97
Seferino Garcia <garciaseferino26@gmail.com>
2023-06-28 02:12:44 -0500

    practiced more functions to solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
 
-};
+    for (i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
+}
+
+console.log(removeFromArray([1,2,3,4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;

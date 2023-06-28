e0d23a2b5617d0ddbede04ce9ad7813afb780dda
Seferino Garcia <garciaseferino26@gmail.com>
2023-06-28 00:46:19 -0500

    practiced writing functions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let ans = '';
+    if (num < 0) return 'ERROR';
+    for (i = 0; i < num; i++) {
+        ans += string;
+    }
+    return ans;
 };
 
 // Do not edit below this line

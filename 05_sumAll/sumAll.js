666ba1ed81a7398e437a8bec9f75775867270aef
Seferino Garcia <garciaseferino26@gmail.com>
2023-06-28 16:31:49 -0500

    practiced functions to pass problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let ans = 0;
 
-};
+    if ((!Number.isInteger(a)) || !Number.isInteger(b)) {
+        return "ERROR";
+    } else if (a <=0 || b <=0) {
+        return "ERROR";
+    }
+
+    let smallN = Math.min(a,b);
+    let bigN = Math.max(a,b);
+
+    for (i = smallN; i <= bigN; i++) {
+        ans += i;
+    }
+
+    return ans
+
+}; 
 
 // Do not edit below this line
 module.exports = sumAll;

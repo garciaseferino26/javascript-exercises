a0deaad7c2eaa5394022e68b6da06ef08d059522
Seferino Garcia <garciaseferino26@gmail.com>
2023-07-07 18:30:15 -0500

    completed the fibonacci problem in jS

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let fibArr = [1,1,2];
+
+    if (number < 3 && number >= 0) {
+        return fibArr[number -1];
+    }
+    if (number < 0) {
+        return "OOPS"
+    }
+    if (isNaN(number)) {
+        let newNumb = parseInt(number)
+        return fibonacci(newNumb);
+    }
+
+    var fibArrLen = fibArr.length;
+
+    while (fibArrLen < number) {
+        fibArr.push(fibArr[fibArrLen - 1] + fibArr[fibArrLen - 2]);
+        fibArrLen += 1;
+    }
+
+    return fibArr[number-1];
+
 
 };
 

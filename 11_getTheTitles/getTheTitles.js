8d86553418f2f603854d2e0b8844c22ca21ca078
Seferino Garcia <garciaseferino26@gmail.com>
2023-07-07 18:42:49 -0500

    practiced traversing array of objects

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let lenArr = array.length;
+    let bookTitles = [];
 
+    for (i = 0; i < lenArr; i++) {
+        bookTitles.push(array[i].title)
+    }
+    return bookTitles
 };
 
 // Do not edit below this line

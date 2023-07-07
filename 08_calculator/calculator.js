dd853accad229969fd5209fa7eb4c20e66b86f52
Seferino Garcia <garciaseferino26@gmail.com>
2023-07-07 00:38:53 -0500

    practiced creating methods in jS

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let ans = 0;
+  let i = 0;
+	while (i < array.length) {
+    ans += array[i];
+    i++;
+  }
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let ans = 1;
+  for (let i = 0; i< args.length; i++) {
+    ans *= args[i];
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let ans = 1;
+  for (let i = number; i > 0; i--) {
+    ans = ans * i;
+  }
+  return ans;
+  
 };
 
 // Do not edit below this line
